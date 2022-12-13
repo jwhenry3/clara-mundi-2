61c833498d9bc8906de2090dadf91b6a2c024a06
Justin Henry <jwhenry123@gmail.com>
2022-12-12 21:55:00 -0600

    started work on quest journal

---
+++ Assets/ClaraMundi/UI/Common/Highlight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ClaraMundi
+{
+    public class Highlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public GameObject Highlighter;
+
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Highlighter.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Highlighter.SetActive(false);
+        }
+    }
+}
