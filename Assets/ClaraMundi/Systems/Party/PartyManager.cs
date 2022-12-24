4014716ba448de9cf0d2822e7a93e87a96da84b8
Justin Henry <jwhenry123@gmail.com>
2022-12-23 18:43:23 -0600

    removed more unused code from party manager

--- Assets/ClaraMundi/Systems/Party/PartyManager.cs
+++ Assets/ClaraMundi/Systems/Party/PartyManager.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace ClaraMundi
 {
     public class PartyManager : MonoBehaviour
     {
-        public readonly Dictionary<string, PartyModel> PartiesByLeader = new();
         public static PartyManager Instance;
         private void Awake()
         {
@@ -15,15 +11,12 @@ namespace ClaraMundi
         }
 
 
-        PartyModel GetPlayerParty(string characterName)
-        {
-            return PartiesByLeader.FirstOrDefault(kvp => kvp.Value.Members.Contains(characterName)).Value;
-        }
 
         public void ServerSendMessage(ChatMessage message)
         {
-            var party = GetPlayerParty(message.SenderCharacterName);
+            var party = PlayerManager.Instance.LocalPlayer.Party.Party;
             if (party == null) return;
+            // send chat message to the party facet
         }
         
 
