7f0eed91b4e07f3387d0987ac81166cc33102cbf
Justin Henry <jwhenry123@gmail.com>
2024-04-12 15:49:36 -0500

    started some auth flows

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,12 +1,15 @@
-import { render } from '@reactunity/renderer';
-import './index.css';
-import Chat from './chat/Chat'
+import { render } from '@reactunity/renderer'
+import './index.css'
+import Chat from './player/Chat'
+import { Auth } from './lobby/Auth'
 
 function App() {
-  return <view className="fixed top-0 bottom-0 left-0 right-0">
-    <text>{`Go to <color=red>src/index.tsx</color> to edit this file`}</text>
-    <Chat />
-  </view>;
+  return (
+    <view className="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center">
+      <Auth />
+      <Chat />
+    </view>
+  )
 }
 
-render(<App />);
+render(<App />)
---
+++ react/src/lobby/Auth.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export function Auth() {
+  const [isRegister, setIsRegister] = useState(false)
+
+  return (
+    <view className="bg-slate-800 w-64 text-white flex flex-col gap-4 p-2">
+      <text className="text-center">Clara Mundi</text>
+      <input placeholder="Username" className="h-8 p-2" />
+      <input placeholder="Password" className="h-8 p-2" />
+      {isRegister && (
+        <input placeholder="Confirm Password" className="h-8 p-2" />
+      )}
+      <view className="flex flex-row gap-4 justify-around">
+        <button className="p-2 px-4 bg-slate-700">
+          <text>{isRegister ? 'Register' : 'Login'}</text>
+        </button>
+        <button className="p-2 px-4">
+          <text>{isRegister ? 'Have Account?' : 'No Account?'}</text>
+        </button>
+      </view>
+    </view>
+  )
+}
